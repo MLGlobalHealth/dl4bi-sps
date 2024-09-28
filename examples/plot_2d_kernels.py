025a1c9737b01805eeaae299c3151a83e4783104
Daniel Jenson <daniel.a.jenson@gmail.com>
2024-09-28 10:28:23 +0100

    updating plotting examples

--- examples/plot_2d_kernels.py
+++ examples/plot_2d_kernels.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
+import jax.numpy as jnp
 import matplotlib.pyplot as plt
 from jax import random
 
 from sps.gp import GP
-from sps.kernels import matern_3_2, matern_5_2, rbf
+from sps.kernels import matern_1_2, matern_3_2, matern_5_2, periodic, rbf
 from sps.priors import Prior
 from sps.utils import build_grid
 
 
 def main():
     rng = random.key(42)
+    rng_sim, rng_noise = random.split(rng)
     s = build_grid(
         [
             {"start": -1.5, "stop": 1.5, "num": 300},
@@ -22,8 +24,14 @@ def main():
     n = len(lengthscales)
     fig, axes = plt.subplots(n, 1, figsize=(5, 5 * n))
     for i, ls in enumerate(lengthscales):
-        gp = GP(matern_5_2, ls=Prior("fixed", {"value": ls}))
-        f, *_ = gp.simulate(rng, s, batch_size, approx)
+        gp = GP(
+            # See https://tinyurl.com/heaton-params
+            matern_1_2,
+            var=Prior("fixed", {"value": 16.41}),
+            ls=Prior("fixed", {"value": ls}),
+        )
+        f, *_ = gp.simulate(rng_sim, s, batch_size, approx)
+        f += jnp.sqrt(0.05) * random.normal(rng_noise, f.shape)
         axes[i].set_title(f"ls={ls}")
         axes[i].imshow(f.squeeze().reshape(300, 500), cmap="Spectral_r")
     plt.tight_layout()
