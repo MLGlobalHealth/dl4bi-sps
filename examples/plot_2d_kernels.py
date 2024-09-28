fixing variance in kernels

examples/plot_2d_kernels.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
+import pandas as pd
 from jax import random
 
 from sps.gp import GP
@@ -9,34 +10,40 @@ from sps.priors import Prior
 from sps.utils import build_grid
 
 
+# Parameters are based on Heaton, et al. benchmark: https://tinyurl.com/heaton-params
 def main():
     rng = random.key(42)
     rng_sim, rng_noise = random.split(rng)
     s = build_grid(
         [
-            {"start": -1.5, "stop": 1.5, "num": 300},
-            {"start": -2.5, "stop": 2.5, "num": 500},
+            {"start": 34.30, "stop": 37.07, "num": 300},
+            {"start": -95.91, "stop": -91.28, "num": 500},
         ]
     )
     batch_size = 1
     approx = True
-    lengthscales = [0.1, 0.2, 0.3]
+    f_mu = 44.49105
+    lengthscales = [0.1, 0.15, 0.2]
     n = len(lengthscales)
     fig, axes = plt.subplots(n, 1, figsize=(5, 5 * n))
     for i, ls in enumerate(lengthscales):
         gp = GP(
-            # See https://tinyurl.com/heaton-params
             matern_1_2,
             var=Prior("fixed", {"value": 16.41}),
             ls=Prior("fixed", {"value": ls}),
         )
         f, *_ = gp.simulate(rng_sim, s, batch_size, approx)
-        f += jnp.sqrt(0.05) * random.normal(rng_noise, f.shape)
+        f += f_mu + jnp.sqrt(0.05) * random.normal(rng_noise, f.shape)
         axes[i].set_title(f"ls={ls}")
         axes[i].imshow(f.squeeze().reshape(300, 500), cmap="Spectral_r")
     plt.tight_layout()
     plt.savefig("2d_gp.png", dpi=150)
     plt.clf()
+    df = pd.DataFrame(
+        jnp.hstack([s.reshape(-1, 2), f[0].reshape(-1, 1)]),
+        columns=["Lat", "Lon", "TrueTemp"],
+    )
+    df.to_csv("mat.csv", index=False)
 
 
 if __name__ == "__main__":

sps/kernels.py
@@ -82,7 +82,7 @@ def rbf(
     Returns:
         A covariance matrix.
     """
-    return var * jnp.exp(-l2_dist_sq(x, y) / (2 * ls**2))
+    return jnp.sqrt(var) * jnp.exp(-l2_dist_sq(x, y) / (2 * ls**2))
 
 
 @jit
@@ -105,15 +105,17 @@ def periodic(
         A covariance matrix.
     """
     x, y = _prepare_dims(x, y)
-    return var * jnp.exp(-2 / ls**2 * jnp.sin(jnp.pi * jnp.abs(x - y.T) / period) ** 2)
+    return jnp.sqrt(var) * jnp.exp(
+        -2 / ls**2 * jnp.sin(jnp.pi * jnp.abs(x - y.T) / period) ** 2
+    )
 
 
 @jit
 def matern_1_2(
     x: ArrayLike,
     y: ArrayLike,
-    variance: float,
-    lengthscale: float,
+    var: float,
+    ls: float,
 ) -> ArrayLike:
     r"""Matern 1/2 kernel.
 
@@ -128,15 +130,15 @@ def matern_1_2(
     """
     d = l2_dist(x, y)
     sqrt3 = 3.0 ** (1 / 2)
-    return variance * (1 + sqrt3 * d / lengthscale) * jnp.exp(-sqrt3 * d / lengthscale)
+    return jnp.sqrt(var) * (1 + sqrt3 * d / ls) * jnp.exp(-sqrt3 * d / ls)
 
 
 @jit
 def matern_3_2(
     x: ArrayLike,
     y: ArrayLike,
-    variance: float,
-    lengthscale: float,
+    var: float,
+    ls: float,
 ) -> ArrayLike:
     r"""Matern 3/2 kernel.
 
@@ -151,15 +153,15 @@ def matern_3_2(
     """
     d = l2_dist(x, y)
     sqrt3 = 3.0 ** (1 / 2)
-    return variance * (1 + sqrt3 * d / lengthscale) * jnp.exp(-sqrt3 * d / lengthscale)
+    return jnp.sqrt(var) * (1 + sqrt3 * d / ls) * jnp.exp(-sqrt3 * d / ls)
 
 
 @jit
 def matern_5_2(
     x: ArrayLike,
     y: ArrayLike,
-    variance: float,
-    lengthscale: float,
+    var: float,
+    ls: float,
 ) -> ArrayLike:
     r"""Matern 5/2 kernel.
 
@@ -176,7 +178,7 @@ def matern_5_2(
     dsq = jnp.square(d)
     sqrt5 = jnp.sqrt(5.0)
     return (
-        variance
-        * (1 + sqrt5 * d / lengthscale + 5 / 3 * dsq / lengthscale**2)
-        * jnp.exp(-sqrt5 * d / lengthscale)
+        jnp.sqrt(var)
+        * (1 + sqrt5 * d / ls + 5 / 3 * dsq / ls**2)
+        * jnp.exp(-sqrt5 * d / ls)
     )