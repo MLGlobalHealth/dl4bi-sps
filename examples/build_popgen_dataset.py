0912cc8bb905fe9d82f2d5b13d24747d9d5938c5
Daniel Jenson <daniel.a.jenson@gmail.com>
2025-07-02 16:37:58 +0100

    adding build popgen dataset

---
+++ examples/build_popgen_dataset.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+
+import numpy as np
+from jax import random
+from tqdm import tqdm
+
+from sps.popgen import PopGen
+
+
+def main(args):
+    rng = random.key(args.seed)
+    popgen = PopGen()
+    N, B, T, C, (H, W) = args.num_batches, args.batch_size, args.num_steps, 1, args.dims
+    mm = np.memmap(args.path, dtype=np.float32, mode="w+", shape=(N, B, T, C, H, W))
+    offset = 0
+    for i in tqdm(range(N), unit="batches"):
+        prevalences, _ = popgen.simulate(
+            rng,
+            args.num_warmup,
+            args.num_steps,
+            args.step_interval,
+            args.batch_size,
+            args.dims,
+            wrap_edges=True,
+        )
+        mm[offset : offset + B] = prevalences
+        mm.flush()
+        offset += B
+    print(f"Finished! Saved to {args.path}")
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(
+        prog=argv[0],
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "-s",
+        "--seed",
+        type=int,
+        default=42,
+        help="Random seed for the entire process.",
+    )
+    parser.add_argument(
+        "-nw",
+        "--num_warmup",
+        type=int,
+        default=1000,
+        help="Number of warmup steps (thrown away) for each set of parameters.",
+    )
+    parser.add_argument(
+        "-ns",
+        "--num_steps",
+        type=int,
+        default=16,
+        help="Number of steps per batch element.",
+    )
+    parser.add_argument(
+        "-si",
+        "--step_interval",
+        type=int,
+        default=16,
+        help="Number of timesteps between kept steps.",
+    )
+    parser.add_argument(
+        "-bs",
+        "--batch_size",
+        type=int,
+        default=32,
+        help="Batch size.",
+    )
+    parser.add_argument(
+        "-nb",
+        "--num_batches",
+        type=int,
+        default=100000,
+        help="Number of batches to generate.",
+    )
+    parser.add_argument(
+        "-d",
+        "--dims",
+        type=list,
+        default=[32, 32],
+        help="Dimensions of surface.",
+    )
+    parser.add_argument(
+        "-p",
+        "--path",
+        type=str,
+        default="popgen.npy",
+        help="Output filename.",
+    )
+    return parser.parse_args(argv[1:])
+
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv)
+    main(args)
